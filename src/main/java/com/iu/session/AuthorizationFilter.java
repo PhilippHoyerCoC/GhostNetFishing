2efa6fb2e3cf210477b3e14c58af711bb7609f51
Philipp Hoyer <philipp.hoyer@codecamp-n.com>
2024-03-02 15:24:46 +0100

    Remove unused imports

--- src/main/java/com/iu/session/AuthorizationFilter.java
+++ src/main/java/com/iu/session/AuthorizationFilter.java
@@ -5,8 +5,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-import java.io.IOException;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
--- src/main/java/com/iu/session/SessionUtils.java
+++ src/main/java/com/iu/session/SessionUtils.java
@@ -1,7 +1,6 @@
 package com.iu.session;
 
 import jakarta.faces.context.FacesContext;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 public class SessionUtils {
